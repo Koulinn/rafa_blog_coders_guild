afebd0c1fba637d9eb059cf9208474ec8fc80de5
Rafael Lima <rafauxdev@gmail.com>
2022-04-18 19:15:43 +0100

    Add basic CRUD operations

--- src/services/posts/index.js
+++ src/services/posts/index.js
@@ -1,47 +1,21 @@
 import express from "express";
 import posts_utils from "./utils.js";
 import POST_SCHEMA from "../../db/Schemas/Posts.js";
-import fetch from "node-fetch";
 import multer from "multer";
 import savePostImage from "../../util/uploadImgs.js";
 
-const { create, saveImgUrl } = posts_utils;
+const { getPosts, create, saveImgUrl, handleLike, editPost } = posts_utils;
 
 const router = express.Router();
 
-router
-  .route("/")
-  .get(async (req, res, next) => {
-    try {
-      const all_posts = await POST_SCHEMA.find();
+router.route("/").get(getPosts).post(create);
 
-      // const omd_res = await fetch(
-      //   `http://www.omdbapi.com/?i=tt3896198&apikey=${process.env.OMDB_API_KEY}`
-      // );
-
-      // const omdb_data = await omd_res.json();
-
-      // console.log(omdb_data);
-      if (all_posts.length > 0) {
-        res.status(200).send({
-          success: true,
-          all_posts,
-        });
-      } else {
-        res.status(404).send({
-          success: false,
-          msg: "No posts found",
-        });
-      }
-    } catch (error) {
-      console.log(error);
-      next();
-    }
-  })
-  .post(create);
+router.route("/like/:postId").put(handleLike);
 
 router
-  .route("/img_upload")
+  .route("/imgUpload/:postId")
   .put(multer({ storage: savePostImage }).single("postImg"), saveImgUrl);
+
+router.route("/:postId").put(editPost);
 
 export default router;
--- src/services/posts/utils.js
+++ src/services/posts/utils.js
@@ -1,10 +1,11 @@
 import POST_SCHEMA from "../../db/Schemas/Posts.js";
+import macaddress from "macaddress";
 
 const create = async (req, res, next) => {
   try {
-    const postBody = req.body;
+    const { body } = req;
 
-    const newPost = new POST_SCHEMA(postBody);
+    const newPost = new POST_SCHEMA(body);
     const savedPost = await newPost.save({ new: true });
 
     if (savedPost._id) {
@@ -20,15 +21,122 @@ const create = async (req, res, next) => {
   }
 };
 
-const saveImgUrl = (req, res, next) => {
+const saveImgUrl = async (req, res, next) => {
   try {
-    console.log(req);
+    const { postId } = req.params;
+    const { path: img_url } = req.file;
+
+    const updatedPost = await POST_SCHEMA.findByIdAndUpdate(
+      postId,
+      { img_url: img_url },
+      { projection: { _id: 1 }, new: true }
+    );
+
+    if (!updatedPost) {
+      res.status(400).send({
+        success: false,
+        msg: "Something went wrong",
+      });
+    } else {
+      res.status(200).send({
+        success: true,
+        updatedPost: updatedPost[0],
+      });
+    }
   } catch (error) {
     console.log(error);
     next();
   }
 };
 
-const posts_utils = { create, saveImgUrl };
+const getPosts = async (req, res, next) => {
+  try {
+    const all_posts = await POST_SCHEMA.find();
+    if (all_posts.length > 0) {
+      res.status(200).send({
+        success: true,
+        all_posts,
+      });
+    } else {
+      res.status(404).send({
+        success: false,
+        msg: "No posts found",
+      });
+    }
+  } catch (error) {
+    console.log(error);
+    next();
+  }
+};
+
+const handleLike = async (req, res, next) => {
+  try {
+    const { postId } = req.params;
+    const mac = await macaddress.one();
+    const isPostLiked = await POST_SCHEMA.find({
+      _id: postId,
+      likes: mac,
+    });
+
+    if (!isPostLiked) {
+      res.status(400).send({
+        success: false,
+        msg: "Something went wrong",
+      });
+    } else if (isPostLiked[0]?.likes.includes(mac)) {
+      const updatedPost = await POST_SCHEMA.findByIdAndUpdate(
+        postId,
+        { $pull: { likes: mac } },
+        { projection: { likes: 1 }, new: true }
+      );
+      const likes = updatedPost.likes.length;
+      res.status(201).send({
+        success: true,
+        likes,
+      });
+    } else {
+      const updatedPost = await POST_SCHEMA.findByIdAndUpdate(
+        postId,
+        { $push: { likes: mac } },
+        { projection: { likes: 1 }, new: true }
+      );
+      const likes = updatedPost.likes.length;
+      res.status(201).send({
+        success: true,
+        likes,
+      });
+    }
+  } catch (error) {
+    console.log(error);
+    next();
+  }
+};
+
+const editPost = async (req, res, next) => {
+  try {
+    const { postId } = req.params;
+    const { body } = req;
+
+    const updatedPost = await POST_SCHEMA.findByIdAndUpdate(postId, body, {
+      new: true,
+    });
+
+    if (updatedPost) {
+      res.status(200).send({
+        success: true,
+        updatedPost: updatedPost,
+      });
+    } else {
+      res.status(400).send({
+        success: false,
+        msg: "Something went wrong",
+      });
+    }
+  } catch (error) {
+    next();
+  }
+};
+
+const posts_utils = { create, saveImgUrl, getPosts, handleLike, editPost };
 
 export default posts_utils;
