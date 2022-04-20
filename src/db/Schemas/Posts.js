e057d048aba0a8ce6e0cb504b849804d14ad5ce6
Rafael Lima <rafauxdev@gmail.com>
2022-04-20 08:08:23 +0100

    Add author code to posts

--- src/db/Schemas/Posts.js
+++ src/db/Schemas/Posts.js
@@ -7,7 +7,9 @@ const POSTS_SCHEMA = new Schema(
         img_url: { type: String },
         text: { type: String, required: true },
         tag: [String],
-        likes: [String]
+        likes: [String],
+        // author: { type: Schema.Types.ObjectId, ref: 'Users' }
+        author_code: { type: String, required: true }
     },
     {
         timestamps: true
